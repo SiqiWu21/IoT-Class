8cf6d3984daa9a04e8badeacad58c66f9358e193
SerenaWu <serenawu@Daisy.local>
2024-08-30 14:50:48 +1000

    update LED part

---
+++ Tutorial5/LED_Serial.py
@@ -0,0 +1,25 @@
+import paho.mqtt.client as mqtt
+import serial
+
+ser = serial.Serial("/dev/rfcomm0", 9600)
+#ser.write(str.encode('Start\r\n'))
+
+def on_connect(client, userdata, flags, rc): # func for making connection
+	print("Connected to MQTT")
+	print("Connection returned result: " + str(rc) )
+	client.subscribe("ifn649")
+	
+def on_message(client, userdata, msg): # Func for Sending msg
+	message = msg.payload.decode()
+	command = f'${message}' #adding $ infront of message as we have substring 1 in the aurdino code
+	ser.write(command.encode())
+	# print(command)
+	#ser.write(str.encode(command))
+	
+		
+	
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("ip-addr", 1883, 60)
+client.loop_forever()
