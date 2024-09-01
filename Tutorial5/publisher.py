e358ee077780bb05774a0545e8b2bb1fe589275b
SerenaWu <serenawu@Daisy.modem>
2024-09-01 23:40:11 +1000

    update

--- Tutorial5/publisher.py
+++
@@ -1,6 +0,0 @@
-import paho.mqtt.publish as publish
-
-publish.single("ifn649","hello world",hostname="13.239.113.88") 
-# Publish a message to the topic "ifn649" on the server - AWS EC2 instance IP
-
-print("done")
