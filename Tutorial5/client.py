3b797d412b81d24ebfd95addbf6fb904247f52f8
SerenaWu <serenawu@Daisy.local>
2024-09-03 14:01:02 +1000

    update

--- Tutorial5/client.py
+++ Tutorial5/client.py
@@ -3,6 +3,7 @@
 #When a message is received, it is sent to the serial port.
 
 import paho.mqtt.client as mqtt
+import serial
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -15,15 +16,17 @@ def on_connect(client, userdata, flags, rc):
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
+    ser = serial.Serial("/dev/rfcomm0",9600)
+    ser.write(msg.payload)
 
-mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-mqttc.on_connect = on_connect
-mqttc.on_message = on_message
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+client.on_connect = on_connect
+client.on_message = on_message
 
-mqttc.connect("13.239.113.88", 1883, 60)    # Connect to the server - AWS EC2 instance IP
+client.connect("13.239.113.88", 1883, 60)    # Connect to the server - AWS EC2 instance IP
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
 # Other loop*() functions are available that give a threaded interface and a
 # manual interface.
-mqttc.loop_forever()
+client.loop_forever()
